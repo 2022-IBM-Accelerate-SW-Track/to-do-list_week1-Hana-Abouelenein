d7362669f82123124d9fc852a29061f875409b36
Hana Abouelenein <hanaabouelenein@hanas-mbp.myfiosgateway.com>
2022-06-09 19:54:50 -0400

    project 1 sumission

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
   
+import "./About.css";
+import profile_pic_name from "../assets/profile_pic_name.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src={profile_pic_name}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Hana Abouelenein</div>
+            <div className="brief_description">
+              <p>Hello! My name is Hana Abouelenein and I am a Computer Science student
+                at the University of Maryland.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
